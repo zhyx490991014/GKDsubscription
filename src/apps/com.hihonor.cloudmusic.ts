e8b047dca9ca87f4db2d574489dabcb27915ca98
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-02-05 23:53:59 +0800

    feat(荣耀音乐): 全屏广告-首页-网易云音乐弹窗广告

---
+++ src/apps/com.hihonor.cloudmusic.ts
@@ -0,0 +1,18 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.hihonor.cloudmusic',
+  name: '荣耀音乐',
+  groups: [
+    {
+      key: 1,
+      name: '全屏广告-首页-网易云音乐弹窗广告',
+      resetMatch: 'app',
+      actionMaximum: 1,
+      activityIds: 'com.netease.cloudmusic.activity.MainActivity',
+      rules: '[vid="dsl_dialog_root"] >4 ImageView[index=1]',
+      exampleUrls: 'https://m.gkd.li/101449500/7471ea5e-df20-4340-971f-1acfccbe6fab',
+      snapshotUrls: 'https://i.gkd.li/import/14167946',
+    },
+  ],
+});
