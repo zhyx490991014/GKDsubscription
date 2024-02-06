07533d9da3f8e5a740f8687db4ee681b92dc0edd
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-02-06 14:34:38 +0800

    feat(ToDesk): 局部广告-广告卡片（补充规则）

--- src/apps/youqu.android.todesk.ts
+++ src/apps/youqu.android.todesk.ts
@@ -6,19 +6,22 @@ export default defineAppConfig({
   groups: [
     {
       key: 1,
-      name: '广告卡片',
+      name: '局部广告-广告卡片',
       desc: '设备列表和连接界面中的广告卡片',
       activityIds: [
         'youqu.android.todesk.activity.WelcomeActivity',
         'youqu.android.todesk.activity.MainActivity', //新增activityIds
       ],
       quickFind: true,
-      rules:
+      rules: [
         '[id="youqu.android.todesk:id/sivDeviceBanner"] + [id="youqu.android.todesk:id/llClose"]',
+        '[vid="layoutMainBottomBanner"] > [vid="ivAdClose"]',
+      ],
       snapshotUrls: [
         'https://i.gkd.li/import/13228546',
         'https://i.gkd.li/import/13256552', //新增快照
         'https://i.gkd.li/import/13296380', //新增快照
+        'https://i.gkd.li/import/14175557',
       ],
     },
   ],
