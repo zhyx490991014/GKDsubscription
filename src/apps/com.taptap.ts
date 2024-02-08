f544c6365fc9a1d334e19bf3ba2719eabd40a655
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-02-08 16:12:31 +0800

    feat(TapTap): 局部广告-游戏浏览界面-推荐广告（删除错误快照、添加prekeys）

--- src/apps/com.taptap.ts
+++ src/apps/com.taptap.ts
@@ -16,23 +16,22 @@ export default defineAppConfig({
     },
     {
       key: 2,
-      name: '游戏浏览界面-推荐广告',
+      name: '局部广告-游戏浏览界面-推荐广告',
       desc: '自动点击【对此内容不感兴趣】',
       rules: [
         {
           key: 0,
           activityIds: 'com.taptap.other.basic.impl.TapMainActivity',
-
           matches:
             '[id="com.taptap.app.middle:id/decision_layout_mask"] + @[id="com.taptap.app.game:id/app_menu"][visibleToUser=true]',
           snapshotUrls: [
-            'https://i.gkd.li/import/12840903',
             'https://i.gkd.li/import/12842279',
             'https://i.gkd.li/import/12864810', //需避免点击正常情况下的app menu
           ],
         },
         {
           key: 1,
+          preKeys: 0,
           activityIds: [
             'com.taptap.game.discovery.impl.findgame.allgame.dialog.FindGameMenuDialog',
             'com.taptap.other.basic.impl.TapMainActivity',
