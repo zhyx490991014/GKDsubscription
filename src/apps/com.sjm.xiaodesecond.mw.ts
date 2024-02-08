3ff7b762340473ee8a6efd33a0753d1e24619736
ZYX <74281356+zhyx490991014@users.noreply.github.com>
2024-02-08 10:37:26 +0800

    feat(喵物次元) 弹窗广告
    太烦人了，一点就误触

--- src/apps/com.sjm.xiaodesecond.mw.ts
+++ src/apps/com.sjm.xiaodesecond.mw.ts
@@ -1,1 +1,38 @@
+import { defineAppConfig } from '../types';
 
+export default defineAppConfig({
+  id: 'com.sjm.xiaodesecond.mw',
+  name: '喵物次元',
+  groups: [
+    {
+      key: 1,
+      name: '弹窗广告',
+      quickFind: true,
+      activityIds: 'com.sjm.xiaodesecond.mw.MainActivity',
+      rules: [
+        {
+          key: 0,
+          name: '弹窗广告-1',
+          desc: '点击左上角跳过',
+          matches:
+            '@TextView[text="跳过"] <<n  ViewGroup >n TextView[text="广告"]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/14203319',
+            'https://i.gkd.li/import/14203087',
+          ],
+        },
+        {
+          key: 1,
+          name: '弹窗广告-2',
+          desc: '点击左上角x',
+          matches:
+            '@ImageView <<2 ViewGroup +2 ViewGroup > TextView[text="广告"]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/14203327',
+            'https://i.gkd.li/import/14203600',
+          ],
+        },
+      ],
+    },
+  ],
+});
