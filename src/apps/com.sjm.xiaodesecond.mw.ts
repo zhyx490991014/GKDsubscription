ec64ff2b5dc5cfb9336a73113c0bd104097fcc3e
ZYX <74281356+zhyx490991014@users.noreply.github.com>
2024-02-08 10:42:01 +0800

    挪desc位置

--- src/apps/com.sjm.xiaodesecond.mw.ts
+++ src/apps/com.sjm.xiaodesecond.mw.ts
@@ -7,13 +7,13 @@ export default defineAppConfig({
     {
       key: 1,
       name: '弹窗广告',
+      desc: '点击左上角x和跳过',
       quickFind: true,
       activityIds: 'com.sjm.xiaodesecond.mw.MainActivity',
       rules: [
         {
           key: 0,
           name: '弹窗广告-1',
-          desc: '点击左上角跳过',
           matches:
             '@TextView[text="跳过"] <<n  ViewGroup >n TextView[text="广告"]',
           snapshotUrls: [
@@ -24,7 +24,6 @@ export default defineAppConfig({
         {
           key: 1,
           name: '弹窗广告-2',
-          desc: '点击左上角x',
           matches:
             '@ImageView <<2 ViewGroup +2 ViewGroup > TextView[text="广告"]',
           snapshotUrls: [
