5818c7aeb53549258160ebb441a146bd9bf63c16
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-02-09 22:35:25 +0800

    feat(华为应用市场): 青少年模式

---
+++ src/apps/com.huawei.appmarket.ts
@@ -0,0 +1,19 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.huawei.appmarket',
+  name: '华为应用市场',
+  groups: [
+    {
+      key: 1,
+      name: '青少年模式',
+      quickFind: true,
+      matchTime: 10000,
+      resetMatch: 'app',
+      actionMaximum: 1,
+      rules:
+        '[text="儿童模式"] <<n [id="android:id/topPanel"] +2 [id="android:id/buttonPanel"] >2 [text="知道了"]',
+      snapshotUrls: 'https://i.gkd.li/import/14222901',
+    },
+  ],
+});
