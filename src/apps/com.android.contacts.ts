d0843e3b51bcb80811cbd6a6d2fbc9e97ac326a0
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-02-18 16:02:15 +0800

    feat(通讯录与拨号): 全屏广告-广告弹窗

---
+++ src/apps/com.android.contacts.ts
@@ -0,0 +1,24 @@
+import { defineAppConfig } from '../types';
+export default defineAppConfig({
+  id: 'com.android.contacts',
+  name: '通讯录与拨号',
+  groups: [
+    {
+      key: 1,
+      name: '全屏广告-广告弹窗',
+      desc: '点击关闭',
+      rules: [
+        {
+          key: 0,
+          quickFind: true,
+          activityIds:
+            'com.mobile.businesshall.ui.common.RecommendPopupActivity',
+          matches: '[vid="img_close_btn"][clickable=true][visibleToUser=true]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/a3ad68fc-4a2e-45e2-87d3-97ece1fceaa4',
+          snapshotUrls: 'https://i.gkd.li/import/14321032',
+        },
+      ],
+    },
+  ],
+});
