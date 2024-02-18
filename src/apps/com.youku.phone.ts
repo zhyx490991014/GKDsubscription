aad1c0ec3326d0d16a24af239609a375c0d70bc1
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-02-18 15:33:30 +0800

    feat(优酷视频): 综合广告-视频播放暂停时的广告

--- src/apps/com.youku.phone.ts
+++ src/apps/com.youku.phone.ts
@@ -6,8 +6,31 @@ export default defineAppConfig({
   deprecatedKeys: [2],
   groups: [
     {
+      key: 1,
+      name: '综合广告-视频播放暂停时的广告',
+      quickFind: true,
+      activityIds: 'com.youku.ui.activity.DetailActivity',
+      rules: [
+        {
+          key: 0,
+          name: '全屏广告、局部广告-xadSDK',
+          matches: '[vid="layout_ad_float"] >n [vid="xadsdk_pause_ad_close"]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/14321469', //局部广告
+            'https://i.gkd.li/import/14321401', //全屏广告
+          ],
+        },
+        {
+          key: 1,
+          name: '局部广告-右下角-广告',
+          matches: '[vid="ad_content"] +2 [vid="novelad_corner_close"]',
+          snapshotUrls: 'https://i.gkd.li/import/14321461',
+        },
+      ],
+    },
+    {
       key: 3,
-      name: '视频推荐弹窗',
+      name: '全屏广告-视频推荐弹窗',
       quickFind: true,
       activityIds: 'com.youku.vip.wrapper.VipHomeActivity',
       rules: '[id="com.youku.phone:id/closeView"][desc="关闭弹层"]',
