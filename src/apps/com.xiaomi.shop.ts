8bdeedff3ff761646f29d96eff4ec443a8d97c43
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-02-24 22:00:04 +0800

    feat(小米商城): 全屏广告-首页弹窗广告

--- src/apps/com.xiaomi.shop.ts
+++ src/apps/com.xiaomi.shop.ts
@@ -6,11 +6,16 @@ export default defineAppConfig({
   groups: [
     {
       key: 1,
-      name: '首页弹窗广告',
-      activityIds: 'com.xiaomi.shop.plugin.homepage.utils.AdDialog',
-      rules:
-        'LinearLayout[childCount=2] > ImageView[id=null] + ImageView[id=null][clickable=true]',
-      snapshotUrls: ['https://i.gkd.li/i/12649330'],
+      name: '全屏广告-首页弹窗广告',
+      activityIds: [
+        'com.xiaomi.shop.plugin.homepage.utils.AdDialog',
+        'com.xiaomi.shop2.activity.MainActivity',
+      ],
+      rules: '[id="android:id/content"] >3 ImageView[index=1][clickable=true]',
+      snapshotUrls: [
+        'https://i.gkd.li/i/12649330',
+        'https://i.gkd.li/i/14393044',
+      ],
     },
   ],
 });
