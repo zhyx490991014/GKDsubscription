e563cf27ab776dd83cb274ba929a64a8da3c8f8c
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-03-03 20:53:56 +0800

    feat(解压专家): 全屏广告-应用推荐广告

--- src/apps/com.fileunzip.zxwknight.ts
+++ src/apps/com.fileunzip.zxwknight.ts
@@ -35,5 +35,18 @@ export default defineAppConfig({
         '@[id="com.fileunzip.zxwknight:id/ziputil_dialog_imageview"] +n [text="升级到专业版"]',
       snapshotUrls: 'https://i.gkd.li/i/13328212',
     },
+    {
+      key: 3,
+      name: '全屏广告-应用推荐广告',
+      rules: [
+        {
+          quickFind: true,
+          matches: '[vid="dialog_recommend_back"]',
+          exampleUrls:
+            'https://m.gkd.li/101449500/e7cf2c1a-9323-4ef4-8c89-82f63244b24b',
+          snapshotUrls: 'https://i.gkd.li/i/14472982',
+        },
+      ],
+    },
   ],
 });
