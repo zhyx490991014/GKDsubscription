39377d9d53afa79645f7d63b47e7cfb55e0fec4b
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-03-11 01:03:08 +0800

    feat(闲鱼): 全屏广告-红包弹窗

--- src/apps/com.taobao.idlefish.ts
+++ src/apps/com.taobao.idlefish.ts
@@ -23,5 +23,21 @@ export default defineAppConfig({
         '@Button[text="取消"] <<n FrameLayout >n TextView[text*="手机信息"]',
       snapshotUrls: 'https://i.gkd.li/i/13620277',
     },
+    {
+      key: 4,
+      name: '全屏广告-红包弹窗',
+      rules: [
+        {
+          key: 0,
+          actionMaximum: 1,
+          resetMatch: 'app',
+          activityIds:
+            'com.taobao.idlefish.maincontainer.activity.MainActivity',
+          matches:
+            'WebView[text="Rax App"] > [id="root"] >6 View[index=2][clickable=true]',
+          snapshotUrls: 'https://i.gkd.li/i/14551046',
+        },
+      ],
+    },
   ],
 });
