ca9e223e69580924ac2deca5b5e35ae001e7677c
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-03-10 10:48:04 +0800

    feat(十六番旅行): 开屏广告、综合广告-广告卡片（补充规则）

--- src/apps/com.fan.app.ts
+++ src/apps/com.fan.app.ts
@@ -5,9 +5,27 @@ export default defineAppConfig({
   name: '十六番旅行',
   groups: [
     {
+      key: 0,
+      name: '开屏广告',
+      global: true,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      matchTime: 10000,
+      rules: [
+        {
+          key: 1,
+          matches: '[vid="LinearLayoutSplAd"] >4 View[clickable=true]',
+          snapshotUrls: 'https://i.gkd.li/i/14548642',
+        },
+      ],
+    },
+    {
       key: 1,
-      name: '广告卡片',
-      activityIds: 'com.mvvm.view.LazyMapStrategyActivity_TengXun',
+      name: '综合广告-广告卡片',
+      activityIds: [
+        'com.mvvm.view.LazyMapStrategyActivity_TengXun',
+        'com.mvc.activity.HomepageActivity2',
+      ],
       rules: [
         {
           key: 0,
@@ -25,11 +43,17 @@ export default defineAppConfig({
           matches: '@Image < View -2 View >2 View > TextView[text="广告"]',
           snapshotUrls: 'https://i.gkd.li/i/13258018',
         },
+        {
+          key: 3,
+          matches:
+            '[vid="relativeLayout3AdContainer"] >7 [desc^="close-fill"] > View[clickable=true]',
+          snapshotUrls: 'https://i.gkd.li/i/14548514',
+        },
       ],
     },
     {
       key: 2,
-      name: '开启通知弹窗',
+      name: '权限提示-通知提示',
       desc: '自动点击“以后再说”',
       quickFind: true,
       matchTime: 10000,
