e6f3381092a30b2ab531092fdace176e068a9da5
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-03-12 20:36:43 +0800

    refactor: 中国移动

--- src/apps/com.greenpoint.android.mc10086.activity.ts
+++ src/apps/com.greenpoint.android.mc10086.activity.ts
@@ -1,34 +1,28 @@
 import { defineAppConfig } from '../types';
-
 export default defineAppConfig({
   id: 'com.greenpoint.android.mc10086.activity',
   name: '中国移动',
+  deprecatedKeys: [1],
   groups: [
     {
-      key: 1,
-      name: '关闭更新弹窗',
-      activityIds: 'com.mc10086.cmcc.view.tabs.AppTabFragment',
-      snapshotUrls: 'https://i.gkd.li/i/12534264',
-      rules:
-        'Button[text="以后再说"][id^="com.greenpoint.android.mc10086.activity:id/dialog_btn"]',
-    },
-    {
       key: 2,
-      name: '首页广告弹窗',
-      activityIds: ['com.mc10086.cmcc.view.tabs.AppTabFragment'],
+      name: '全屏广告-首页弹窗广告',
+      activityIds: 'com.mc10086.cmcc.view.tabs.AppTabFragment',
       rules: [
         {
           matches:
             '[id="com.greenpoint.android.mc10086.activity:id/ad_image"] < RelativeLayout + [id="com.greenpoint.android.mc10086.activity:id/close_btn"]',
-          snapshotUrls: ['https://i.gkd.li/i/12662361'],
+          snapshotUrls: 'https://i.gkd.li/i/12662361',
         },
       ],
     },
     {
       key: 10,
-      name: '请求推送通知弹窗',
+      name: '通知提示-请求推送通知弹窗',
       desc: '请求推送通知弹窗，点击取消',
-      activityIds: 'com.mc10086.cmcc.base.StartPageActivity',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
       rules: [
         {
           matches: '[text*="开启推送通知"] +(2) LinearLayout > [text="取消"]',
@@ -40,15 +34,15 @@ export default defineAppConfig({
         {
           quickFind: true,
           matches: '[text="授权提醒"] +3 [text="拒绝"]',
-          snapshotUrls: ['https://i.gkd.li/i/13775652'],
+          snapshotUrls: 'https://i.gkd.li/i/13775652',
         },
       ],
     },
     {
       key: 11,
-      name: '请求获取剪贴板权限弹窗',
+      name: '权限提示-请求获取剪贴板权限弹窗',
       desc: '请求获取剪贴板权限弹窗，点击不允许',
-      activityIds: ['com.mc10086.cmcc.view.tabs.AppTabFragment'],
+      activityIds: 'com.mc10086.cmcc.view.tabs.AppTabFragment',
       rules: [
         {
           matches:
@@ -62,25 +56,36 @@ export default defineAppConfig({
     },
     {
       key: 12,
-      name: '右侧悬浮小图标',
-      activityIds: ['com.mc10086.cmcc.view.tabs.AppTabFragment'],
+      name: '局部广告-右侧悬浮小图标',
+      quickFind: true,
+      activityIds: 'com.mc10086.cmcc.view.tabs.AppTabFragment',
       rules: [
         {
+          key: 0,
           matches:
-            '[id="com.greenpoint.android.mc10086.activity:id/drag_img"] + [id="com.greenpoint.android.mc10086.activity:id/close_btn"]',
-          snapshotUrls: ['https://i.gkd.li/i/12662265'],
+            '[id="com.greenpoint.android.mc10086.activity:id/close_btn"]',
+          snapshotUrls: 'https://i.gkd.li/i/12662265',
+        },
+        {
+          key: 1,
+          matches: '[vid="close_btn_bottom"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/276c7811-52f7-4379-8782-f9fb0b6cec1c',
+          snapshotUrls: 'https://i.gkd.li/i/14570369',
         },
       ],
     },
     {
       key: 13,
-      name: '请求好评弹窗',
-      activityIds: ['com.mc10086.cmcc.view.tabs.AppTabFragment'],
+      name: '评价提示-请求好评弹窗',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
       rules: [
         {
           matches:
             '@[id="com.greenpoint.android.mc10086.activity:id/close_img"][desc="关闭"] - RelativeLayout >(2) [text$="好评"]',
-          snapshotUrls: ['https://i.gkd.li/i/12662345'],
+          snapshotUrls: 'https://i.gkd.li/i/12662345',
         },
       ],
     },
