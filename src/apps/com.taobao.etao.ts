bf4f8027d8b2cfb0e2812a49e64689f9982d4275
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-03-16 13:29:38 +0800

    feat(一淘): 全屏广告-弹窗广告（补充规则）

--- src/apps/com.taobao.etao.ts
+++ src/apps/com.taobao.etao.ts
@@ -7,7 +7,7 @@ export default defineAppConfig({
   groups: [
     {
       key: 2,
-      name: '弹窗广告',
+      name: '全屏广告-弹窗广告',
       rules: [
         {
           key: 0,
@@ -22,21 +22,31 @@ export default defineAppConfig({
             'https://i.gkd.li/i/13670025',
           ],
         },
+        {
+          key: 1,
+          activityIds: 'com.taobao.etao.app.homev4.HomeV4Activity',
+          matches:
+            '[text="一淘-首页红包升级-2312wf"] >5 View[childCount=3] > View[index=0][visibleToUser=true]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/db5c7761-3a8b-4bc6-a61c-65dd30f61b9f',
+          snapshotUrls: 'https://i.gkd.li/i/14622468',
+        },
       ],
     },
     {
       key: 10,
-      name: '请求打开系统通知提示信息',
-      desc: '自动点击提示信息的x按钮',
+      name: '通知提示-请求打开系统通知提示信息',
+      desc: '自动点击提示信息的 x 按钮',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
       rules: [
         {
-          activityIds: ['com.taobao.sns.app.message.MessageActivity'],
           matches:
             '[text^="打开消息通知"] + [text="去开启"] + ImageView[clickable=true]',
           snapshotUrls: ['https://i.gkd.li/i/12684278'],
         },
         {
-          activityIds: ['com.taobao.etao.mine.MetaXMineActivity'],
           matches:
             '[text^="打开消息通知"] + FrameLayout > [text="去开启"] < FrameLayout + ImageView[clickable=true]',
           snapshotUrls: ['https://i.gkd.li/i/12684351'],
