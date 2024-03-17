f2feb8a429282046334c8ab4400a13b10fee0a04
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-03-17 11:04:18 +0800

    perf: 全局系统应用白名单更新

---
+++ src/globalDefaultApp.ts
@@ -0,0 +1,78 @@
+export const commonAppBlackList: string[] = [
+  // 在一些非系统应用中禁用
+  'com.tencent.mm', // 微信
+  'li.songe.gkd', // GKD
+  'com.eg.android.AlipayGphone', //支付宝
+  'com.tencent.wetype', // 微信输入法
+  'com.tmri.app.main', // 交管 12123
+  'com.github.android', // GitHub
+  'org.telegram.messenger', // Telegram
+  'com.twitter.android', // X(推特)
+  'com.aistra.hail', // 雹
+  'com.heyanle.easybangumi4', // 纯纯看番
+  'com.perol.pixez', // PixEz
+  'com.perol.play.pixez', // PixEz Google Play版
+  'com.magicalstory.videos', // 奇妙影视
+  'com.guozhigq.pilipala', // PiliPala
+  'com.salt.music', // Salt Player
+  'com.xuncorp.qinalt.music', // 青盐云听
+  'com.github.wgh136.pica_comic', // Pica Comic
+  'com.yenaly.han1meviewer', // Han1meViewer
+  'ru.zdevs.zarchiver', // ZArchiver
+  'ru.zdevs.zarchiver.pro', // ZArchiver Pro
+  'me.zhanghai.android.files', // 质感文件
+  'com.xjcheng.musictageditor', // 音乐标签
+  'com.lalilu.lmusic', // LMusic
+  'com.ktls.fileinfo', // 存储空间清理
+  'idm.internet.download.manager', // 1DM
+  'idm.internet.download.manager.plus', // 1DM+
+  'com.mxtech.videoplayer', // MX播放器
+  'com.mxtech.videoplayer.pro', // MX播放器专业版
+  'com.github.kr328.clash', // Clash for Android
+  'com.x8bit.bitwarden', // Bitwarden
+  'org.localsend.localsend_app', // LocalSend
+  'moe.shizuku.privileged.api', // Shizuku
+  'com.ktls.automation', // 自动点击工具
+  'com.deepl.mobiletranslator', // DeepL
+  'com.accessibilitymanager', // 无障碍管理器
+  'me.tasy5kg.cutegif', // 小萌 GIF
+  'com.absinthe.libchecker', // LibChecker
+  'com.github.metacubex.clash.meta', // Clash Meta for Android
+  'io.github.jd1378.otphelper', // 短信验证码自动复制
+  'com.valvesoftware.android.steam.community', // Steam
+  'io.legado.app.release', // 阅读
+  'com.getsurfboard', // Surfboard
+  'moe.nb4a', // NekoBox
+  'com.v2ray.ang', // v2rayNG
+  'com.xiaoyv.bangumi', // Bangumi for Android
+  'com.funny.translation', // 译站
+  'com.looker.droidify', // Droid-ify
+  'app.eleven.com.fastfiletransfer', // 文件闪传
+  'com.idaodan.clean.master', // 雪豹速清
+  'com.samsung.agc.gcam84', // 相机
+  'com.wsw.cospa', // 异次元
+  'app.mihon', // Mihon
+
+  // 系统自带非系统应用
+  // 小米
+  'com.miui.huanji', // 小米换机
+  'com.miui.weather2', // 小米天气
+  'com.miui.calculator', // 小米计算器
+  'com.android.soundrecorder', // 小米录音机
+  'com.android.email', // 小米电子邮件
+  'com.miui.screenrecorder', // 小米屏幕录制
+
+  // https://github.com/gkd-kit/gkd/issues/560
+  'com.android.deskclock', // 时钟
+
+  // https://github.com/gkd-kit/gkd/issues/451
+  'mark.via', // via浏览器
+  'mark.via.gp', // via浏览器Google Play版
+  'com.mmbox.xbrowser', // X浏览器
+  'com.mmbox.xbrowser.pro', // X浏览器Google Play版
+  'com.mycompany.app.soulbrowser', // soul浏览器
+];
+
+export const systemAppWhiteList: string[] = [
+  // 在一些系统软件中启用
+];
--- src/globalGroups.ts
+++ src/globalGroups.ts
@@ -2,86 +2,7 @@ import apps from './rawApps';
 import { RawGlobalGroup } from '@gkd-kit/api';
 import { RawApp, RawAppGroup } from './types';
 import * as utils from './utils';
-
-const diabledAppIds: string[] = [
-  // 在一些非系统应用中禁用
-  'com.tencent.mm', // 微信
-  'li.songe.gkd', // GKD
-  'com.eg.android.AlipayGphone', //支付宝
-  'com.tencent.wetype', // 微信输入法
-  'com.tmri.app.main', // 交管 12123
-  'com.github.android', // GitHub
-  'org.telegram.messenger', // Telegram
-  'com.twitter.android', // X(推特)
-  'com.aistra.hail', // 雹
-  'com.heyanle.easybangumi4', // 纯纯看番
-  'com.perol.pixez', // PixEz
-  'com.perol.play.pixez', // PixEz Google Play版
-  'com.magicalstory.videos', // 奇妙影视
-  'com.guozhigq.pilipala', // PiliPala
-  'com.salt.music', // Salt Player
-  'com.xuncorp.qinalt.music', // 青盐云听
-  'com.github.wgh136.pica_comic', // Pica Comic
-  'com.yenaly.han1meviewer', // Han1meViewer
-  'ru.zdevs.zarchiver', // ZArchiver
-  'ru.zdevs.zarchiver.pro', // ZArchiver Pro
-  'me.zhanghai.android.files', // 质感文件
-  'com.xjcheng.musictageditor', // 音乐标签
-  'com.lalilu.lmusic', // LMusic
-  'com.ktls.fileinfo', // 存储空间清理
-  'idm.internet.download.manager', // 1DM
-  'idm.internet.download.manager.plus', // 1DM+
-  'com.mxtech.videoplayer', // MX播放器
-  'com.mxtech.videoplayer.pro', // MX播放器专业版
-  'com.github.kr328.clash', // Clash for Android
-  'com.x8bit.bitwarden', // Bitwarden
-  'org.localsend.localsend_app', // LocalSend
-  'moe.shizuku.privileged.api', // Shizuku
-  'com.ktls.automation', // 自动点击工具
-  'com.deepl.mobiletranslator', // DeepL
-  'com.accessibilitymanager', // 无障碍管理器
-  'me.tasy5kg.cutegif', // 小萌 GIF
-  'com.absinthe.libchecker', // LibChecker
-  'com.github.metacubex.clash.meta', // Clash Meta for Android
-  'io.github.jd1378.otphelper', // 短信验证码自动复制
-  'com.valvesoftware.android.steam.community', // Steam
-  'io.legado.app.release', // 阅读
-  'com.getsurfboard', // Surfboard
-  'moe.nb4a', // NekoBox
-  'com.v2ray.ang', // v2rayNG
-  'com.xiaoyv.bangumi', // Bangumi for Android
-  'com.funny.translation', // 译站
-  'com.looker.droidify', // Droid-ify
-  'app.eleven.com.fastfiletransfer', // 文件闪传
-  'com.idaodan.clean.master', // 雪豹速清
-  'com.samsung.agc.gcam84', // 相机
-  'com.wsw.cospa', // 异次元
-  'app.mihon', // Mihon
-
-  // 系统自带非系统应用
-  // 小米
-  'com.miui.huanji', // 小米换机
-  'com.miui.weather2', // 小米天气
-  'com.miui.calculator', // 小米计算器
-  'com.android.soundrecorder', // 小米录音机
-  'com.android.email', // 小米电子邮件
-  'com.miui.screenrecorder', // 小米屏幕录制
-
-  // https://github.com/gkd-kit/gkd/issues/560
-  'com.android.deskclock', // 时钟
-
-  // https://github.com/gkd-kit/gkd/issues/451
-  'mark.via', // via浏览器
-  'mark.via.gp', // via浏览器Google Play版
-  'com.mmbox.xbrowser', // X浏览器
-  'com.mmbox.xbrowser.pro', // X浏览器Google Play版
-  'com.mycompany.app.soulbrowser', // soul浏览器
-];
-
-// 系统软件全局启用
-const enabledAppIds: string[] = [
-  // 在一些系统软件中启用
-];
+import { commonAppBlackList, systemAppWhiteList } from './globalDefaultApp';
 
 // 如果应用规则已有全局规则中的某一类的规则, 则在对应全局规则禁用此应用
 function filterAppsByGroup(apps: RawApp[], groupNamePrefix: string): string[] {
@@ -96,28 +17,29 @@ function filterAppsByGroup(apps: RawApp[], groupNamePrefix: string): string[] {
 
 // 设置单独禁用
 const openDiabledAppIds = new Set([
-  ...diabledAppIds,
+  ...commonAppBlackList,
   ...filterAppsByGroup(apps, '开屏广告'),
 ]);
 const updateDiabledAppIds = new Set([
-  ...diabledAppIds,
+  ...commonAppBlackList,
   ...filterAppsByGroup(apps, '更新提示'),
 ]);
 const youngDiabledAppIds = new Set([
-  ...diabledAppIds,
+  ...commonAppBlackList,
   'xxx.pornhub.fuck', // JavDB
   ...filterAppsByGroup(apps, '青少年模式'),
 ]);
 
 // 设置系统软件单独启用
 const openEnabledAppIds = new Set([
-  ...enabledAppIds,
+  ...systemAppWhiteList,
   'com.bbk.theme', // i 主题
   'com.sec.android.app.samsungapps', // 三星应用商店
   'com.bbk.appstore', // vivo应用商店
+  'com.xiaomi.market', // 小米应用商店
 ]);
-const updateEnabledAppIds = new Set([...enabledAppIds]);
-const youngEnabledAppIds = new Set([...enabledAppIds]);
+const updateEnabledAppIds = new Set([...systemAppWhiteList]);
+const youngEnabledAppIds = new Set([...systemAppWhiteList]);
 
 const globalGroups: RawGlobalGroup[] = [
   {
