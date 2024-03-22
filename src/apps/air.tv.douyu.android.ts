09f7b6523bfbb98dd0476fbeeb29a696c33db06e
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-03-22 19:11:21 +0800

    feat(斗鱼): 局部广告-直播间广告（补充规则）

--- src/apps/air.tv.douyu.android.ts
+++ src/apps/air.tv.douyu.android.ts
@@ -8,12 +8,15 @@ export default defineAppConfig({
     {
       key: 3,
       name: '局部广告-直播间广告',
-      activityIds: 'tv.douyu.view.activity.PlayerActivity',
+      activityIds: [
+        'tv.douyu.view.activity.PlayerActivity',
+        'com.douyu.module.player.p.rambobase.RamboPlayerActivity',
+      ],
       rules: [
         {
           key: 0,
           matches:
-            'ImageView < ViewGroup + @ViewGroup[childCount=1] > ImageView', //添加childCount=1避免【全屏播放-设置】误触
+            'ImageView < ViewGroup + @ViewGroup[childCount=1] > ImageView', //添加 childCount=1 避免"全屏播放-设置"误触
           snapshotUrls: [
             'https://i.gkd.li/i/12892825',
             'https://i.gkd.li/i/13037239',
@@ -22,10 +25,7 @@ export default defineAppConfig({
         {
           key: 1,
           matches: 'ViewPager[childCount=3] + @View + ImageView + View',
-          snapshotUrls: [
-            'https://i.gkd.li/i/12892825',
-            'https://i.gkd.li/i/14208369', //不限定childCount=3误触
-          ],
+          snapshotUrls: 'https://i.gkd.li/i/12892825',
         },
         {
           key: 2,
@@ -44,6 +44,31 @@ export default defineAppConfig({
           matches:
             'FrameLayout[childCount=2] > @ImageView - FrameLayout > RelativeLayout > WebView',
           snapshotUrls: 'https://i.gkd.li/i/13056107',
+        },
+        {
+          key: 5,
+          matches:
+            'ViewGroup[childCount=8] > ViewGroup[index=5] >4 RelativeLayout[visibleToUser=true] > ImageView[clickable=true][index=1]',
+          snapshotUrls: [
+            'https://i.gkd.li/i/13948990',
+            'https://i.gkd.li/i/14310474', // 避免在此页面误触
+          ],
+        },
+        {
+          key: 6,
+          matches:
+            '[id="android:id/content"] > RelativeLayout >4 FrameLayout + ImageView',
+          snapshotUrls: [
+            'https://i.gkd.li/i/14668232',
+            'https://i.gkd.li/i/14668248',
+            'https://i.gkd.li/i/14667860',
+          ],
+        },
+        {
+          key: 7,
+          quickFind: true,
+          matches: '@ImageView[visibleToUser=true] -2 * >3 [text="推荐"]',
+          snapshotUrls: 'https://i.gkd.li/i/14668232',
         },
       ],
     },
