f8bf52eb5050f97cb815b9b5e97d4fab62dc9bfe
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-03-22 19:43:30 +0800

    refactor: 小米音乐

--- src/apps/com.miui.player.ts
+++ src/apps/com.miui.player.ts
@@ -6,7 +6,7 @@ export default defineAppConfig({
   groups: [
     {
       key: 1,
-      name: '浮窗广告',
+      name: '局部广告-浮窗广告',
       desc: '关闭右侧飘窗广告',
       quickFind: true,
       activityIds:
@@ -17,27 +17,41 @@ export default defineAppConfig({
     },
     {
       key: 2,
-      name: '横幅广告',
+      name: '局部广告-横幅广告',
       desc: '关闭播放页面横幅广告',
       quickFind: true,
       activityIds:
         'com.tencent.qqmusiclite.activity.player.MusicPlayerActivity',
-      rules: '[id="com.miui.player:id/ad_skip_text"][text="关闭"]',
-      snapshotUrls: [
-        'https://i.gkd.li/i/13304347', // 倒计时
-        'https://i.gkd.li/i/13304344', // 可关闭
+      rules: [
+        {
+          key: 0,
+          matches: '[id="com.miui.player:id/ad_skip_text"][text="关闭"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/d923ba1b-3098-49b0-8593-c84dbaa16612',
+          snapshotUrls: [
+            'https://i.gkd.li/i/13304347', // 倒计时
+            'https://i.gkd.li/i/13304344', // 可关闭
+          ],
+        },
+        {
+          key: 1,
+          matches: '[text="广告｜跳过"]', // 不使用vid="ad_skip_text"，避免和 key 0 冲突
+          exampleUrls:
+            'https://m.gkd.li/57941037/2ae1ca24-fb50-48ff-9343-e5607b512127',
+          snapshotUrls: 'https://i.gkd.li/i/14711960',
+        },
       ],
     },
     {
       key: 3,
-      name: '弹窗广告_底部弹窗',
+      name: '全屏广告-看广告免费听弹窗',
       activityIds: 'com.tencent.qqmusiccommon.hybrid.HybridViewActivity',
       rules: 'ViewGroup > @ViewGroup + ViewGroup[childCount=5]',
       snapshotUrls: ['https://i.gkd.li/i/13304343'],
     },
     {
       key: 4,
-      name: '飘窗广告_首页右侧',
+      name: '局部广告-首页右侧悬浮卡片',
       activityIds: 'com.tencent.qqmusiclite.activity.MainActivity',
       quickFind: true,
       rules:
@@ -46,7 +60,8 @@ export default defineAppConfig({
     },
     {
       key: 5,
-      name: '首页-弹窗广告',
+      name: '功能类-年度报告邀请函弹窗',
+      desc: '点击关闭',
       activityIds: 'com.tencent.qqmusiclite.activity.MainActivity',
       quickFind: true,
       rules: '[id="com.miui.player:id/iv_close_dialog_button"]',
@@ -54,14 +69,14 @@ export default defineAppConfig({
     },
     {
       key: 10,
-      name: '会员过期续费弹窗',
+      name: '全屏广告-会员过期续费弹窗',
       activityIds: 'com.tencent.qqmusiccommon.hybrid.HybridViewActivity',
       rules: '[id=null][desc="关闭弹框按钮"][clickable=true]',
       snapshotUrls: 'https://i.gkd.li/i/12700955',
     },
     {
       key: 11,
-      name: '我的页面-顶部 banner 广告',
+      name: '局部广告-"我的"页面顶部卡片广告',
       activityIds: 'com.tencent.qqmusiclite.activity.MainActivity',
       rules:
         '[id="com.miui.player:id/banner_image"] + [id="com.miui.player:id/close_banner"]',
@@ -69,8 +84,8 @@ export default defineAppConfig({
     },
     {
       key: 12,
-      name: '看广告视频领VIP',
-      desc: '等待15s自动点击【退出】',
+      name: '功能类-看广告视频领VIP',
+      desc: '等待 15s 自动点击"退出"',
       rules: [
         {
           actionDelay: 15000,
