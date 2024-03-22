a371fd3730a75cd30478a119f89de550f38e3b04
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-03-22 19:28:19 +0800

    refactor: 京东

--- src/apps/com.jingdong.app.mall.ts
+++ src/apps/com.jingdong.app.mall.ts
@@ -3,17 +3,18 @@ import { defineAppConfig } from '../types';
 export default defineAppConfig({
   id: 'com.jingdong.app.mall',
   name: '京东',
+  deprecatedKeys: [7],
   groups: [
     {
       key: 1,
-      name: '购物车界面-砸金蛋',
+      name: '局部广告-购物车页面砸金蛋',
       activityIds: 'com.jingdong.app.mall.MainFrameActivity',
       rules: '[id="com.jd.lib.cart.feature:id/iv_egg_close"]',
       snapshotUrls: 'https://i.gkd.li/i/12642266',
     },
     {
       key: 2,
-      name: '我的界面-悬浮广告',
+      name: '局部广告-我的页面悬浮广告',
       activityIds: 'com.jingdong.app.mall.MainFrameActivity',
       rules: [
         {
@@ -37,7 +38,7 @@ export default defineAppConfig({
     },
     {
       key: 3,
-      name: '首页-右侧浮层广告',
+      name: '局部广告-首页右侧浮层广告',
       activityIds: 'com.jingdong.app.mall.MainFrameActivity',
       rules: 'RelativeLayout >n * > [desc^="关闭浮层"]',
       snapshotUrls: [
@@ -50,30 +51,33 @@ export default defineAppConfig({
     {
       key: 4,
       name: '通知提示-请求打开通知',
-      activityIds: [
-        'com.jd.lib.message.messagecenter.view.activity.MessageCenterMainActivityNew',
-        'com.jd.lib.ordercenter.mygoodsorderlist.view.activity.MyOrderListActivity',
-        'com.jingdong.app.mall.MainFrameActivity',
-      ],
+      desc: '点击关闭',
+      quickFind: true,
+      //matchTime: 10000, 该弹窗可能在多个页面出现
+      actionMaximum: 1,
+      resetMatch: 'app',
+      actionMaximumKey: 0,
       rules: [
         {
-          matches: 'TextView[text="开启消息通知"] +n ImageView[clickable=true]',
+          key: 0,
+          matches:
+            '@ImageView[clickable=true][visibleToUser=true] <n * > [text="开启消息通知"]',
           snapshotUrls: [
-            'https://i.gkd.li/i/12839864', //com.jd.lib.message.messagecenter.view.activity.MessageCenterMainActivityNew
-            'https://i.gkd.li/i/13772299', //com.jd.lib.ordercenter.mygoodsorderlist.view.activity.MyOrderListActivity
-            'https://i.gkd.li/i/13917163', //com.jingdong.app.mall.MainFrameActivity
+            'https://i.gkd.li/i/13917163',
+            'https://i.gkd.li/i/13463618',
+            'https://i.gkd.li/i/14692570',
           ],
         },
         {
-          matches:
-            'TextView[text ^="打开系统通知"] - FrameLayout[clickable=true]',
+          key: 1,
+          matches: '@[clickable=true] + [text^="打开系统通知"]',
           snapshotUrls: 'https://i.gkd.li/i/12839865',
         },
       ],
     },
     {
       key: 5,
-      name: '广告弹窗',
+      name: '全屏广告-广告弹窗',
       rules: [
         {
           key: 0,
@@ -113,7 +117,7 @@ export default defineAppConfig({
     },
     {
       key: 6,
-      name: '首页-底部横幅广告',
+      name: '局部广告-首页底部横幅广告',
       activityIds: 'com.jingdong.app.mall.MainFrameActivity',
       rules: 'ImageView < FrameLayout - FrameLayout > ImageView[desc="关闭"]',
       snapshotUrls: [
@@ -123,30 +127,17 @@ export default defineAppConfig({
       ],
     },
     {
-      key: 7,
-      name: '首页弹窗-打开通知',
-      desc: '自动点击正下方的【x】',
-      quickFind: true,
-      rules: [
-        {
-          activityIds: 'com.jingdong.app.mall.MainFrameActivity',
-          matches: '[id="com.jingdong.app.mall:id/btn_close"]',
-          snapshotUrls: 'https://i.gkd.li/i/13463618',
-        },
-      ],
-    },
-    {
       key: 10,
-      name: '京东账号登录授权',
-      desc: '自动点击【确认登录】',
+      name: '功能类-京东账号登录授权',
+      desc: '自动点击"确认登录"',
       activityIds: 'com.jingdong.app.mall.WebActivity',
       rules: '[text="京东登录"] > [desc="确认登录"]',
       snapshotUrls: 'https://i.gkd.li/i/12901734',
     },
     {
       key: 11,
-      name: '支付界面-产品推荐',
-      desc: '自动点击右上角【x】',
+      name: '局部广告-支付页面产品推荐',
+      desc: '自动点击右上角"x"',
       activityIds:
         'com.jd.lib.settlement.fillorder.activity.NewFillOrderActivity',
       rules: 'ImageView[clickable=true && desc="关闭"]',
@@ -154,8 +145,8 @@ export default defineAppConfig({
     },
     {
       key: 12,
-      name: '购物车界面-支付成功广告弹窗',
-      desc: '自动点击正下方的【x】',
+      name: '全屏广告-购物车页面支付成功广告弹窗',
+      desc: '自动点击正下方的"x"',
       quickFind: true,
       rules: [
         {
