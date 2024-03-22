61d57426b0daa8967ff21289fa52d4c021120aa6
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-03-23 00:04:45 +0800

    feat(酷狗音乐): 全屏广告-[看广告免费听歌]弹窗

--- src/apps/com.kugou.android.ts
+++ src/apps/com.kugou.android.ts
@@ -33,7 +33,7 @@ export default defineAppConfig({
     },
     {
       key: 1,
-      name: '个人页顶部广告',
+      name: '局部广告-个人页顶部广告',
       rules: [
         {
           activityIds: 'com.kugou.android.app.MediaActivity',
@@ -47,7 +47,7 @@ export default defineAppConfig({
     },
     {
       key: 2,
-      name: 'VIP弹窗',
+      name: '全屏广告-VIP弹窗',
       rules: [
         {
           matches:
@@ -55,6 +55,20 @@ export default defineAppConfig({
           exampleUrls:
             'https://m.gkd.li/87047583/84c1379f-5eb1-4982-b27b-35e267594101',
           snapshotUrls: 'https://i.gkd.li/i/13548005',
+        },
+      ],
+    },
+    {
+      key: 3,
+      name: '全屏广告-[看广告免费听歌]弹窗',
+      desc: '点击关闭',
+      rules: [
+        {
+          activityIds: 'com.kugou.android.app.MediaActivity',
+          matches: '[desc="看广告，免费听会员歌"] +2 [desc="关闭"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/dfd70c7c-3200-493e-9afa-404de5fcd2ff',
+          snapshotUrls: 'https://i.gkd.li/i/14722104',
         },
       ],
     },
