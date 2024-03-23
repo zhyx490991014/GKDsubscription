2784281ea0e3bb0a933fdafc6846510fc33fa7e9
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-03-23 23:04:58 +0800

    feat(鱼泡网): 局部广告-信息流广告（补充规则）

--- src/apps/io.dcloud.H576E6CC7.ts
+++ src/apps/io.dcloud.H576E6CC7.ts
@@ -7,10 +7,21 @@ export default defineAppConfig({
     {
       key: 1,
       name: '局部广告-信息流广告',
-      quickFind: true,
-      activityIds: 'com.yupao.entry.MainActivity',
-      rules: '[vid="ad_close"]',
-      snapshotUrls: 'https://i.gkd.li/i/14731762',
+      rules: [
+        {
+          key: 0,
+          quickFind: true,
+          activityIds: 'com.yupao.entry.MainActivity',
+          matches: '[vid="ad_close"]',
+          snapshotUrls: 'https://i.gkd.li/i/14731762',
+        },
+        {
+          key: 1,
+          activityIds: 'com.yupao.entry.MainActivity',
+          matches: '[text="广告"] <2 View +3 View > TextView[clickable=true]',
+          snapshotUrls: 'https://i.gkd.li/i/14732000',
+        },
+      ],
     },
     {
       key: 2,
