fe13c7e8495a34aec50fe2bb5965ff9d14bdd4c2
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-03-23 14:11:57 +0800

    feat(腾讯动漫): 局部广告-信息流广告

--- src/apps/com.qq.ac.android.ts
+++ src/apps/com.qq.ac.android.ts
@@ -7,11 +7,28 @@ export default defineAppConfig({
   groups: [
     {
       key: 2,
-      name: '漫画阅读页面-左侧悬浮广告',
+      name: '局部广告-漫画阅读页面-左侧悬浮广告',
       activityIds: 'com.qq.ac.android.reader.comic.ComicReaderActivity',
       rules:
         '[id="com.qq.ac.android:id/operation_ad_view"] > [id="com.qq.ac.android:id/btn_close"]',
-      snapshotUrls: ['https://i.gkd.li/i/12707756'],
+      snapshotUrls: 'https://i.gkd.li/i/12707756',
+    },
+    {
+      key: 3,
+      name: '局部广告-信息流广告',
+      desc: '点击关闭',
+      rules: [
+        {
+          key: 0,
+          name: '漫画章节之间的广告',
+          quickFind: true,
+          activityIds: 'com.qq.ac.android.reader.comic.ComicReaderActivity',
+          matches: '[vid="iv_del"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/1be7564d-f6c2-429d-9cb0-3d923adafc8a',
+          snapshotUrls: 'https://i.gkd.li/i/14730335',
+        },
+      ],
     },
   ],
 });
