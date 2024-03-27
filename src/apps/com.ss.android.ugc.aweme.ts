f584d541b93309eeae32515728d93d0bc2de26fa
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-03-27 22:14:17 +0800

    feat(抖音): 功能类-[添加快捷方式到桌面]弹窗（补充 activityid）

--- src/apps/com.ss.android.ugc.aweme.ts
+++ src/apps/com.ss.android.ugc.aweme.ts
@@ -172,11 +172,13 @@ export default defineAppConfig({
     },
     {
       key: 13,
-      name: '功能类-添加快捷方式到桌面',
+      name: '功能类-[添加快捷方式到桌面]弹窗',
+      desc: '关闭弹窗',
       actionMaximum: 1,
       resetMatch: 'app',
       rules: [
         {
+          key: 0,
           name: '搜索快捷方式',
           activityIds:
             'com.ss.android.ugc.aweme.search.activity.SearchResultActivity',
@@ -185,11 +187,28 @@ export default defineAppConfig({
           snapshotUrls: 'https://i.gkd.li/i/13338556',
         },
         {
+          key: 1,
           name: '商城快捷方式',
-          activityIds: 'com.ss.android.ugc.aweme.main.MainActivity',
+          quickFind: true,
+          activityIds: [
+            'com.ss.android.ugc.aweme.main.MainActivity',
+            'com.ss.android.ugc.aweme.live.LiveDummyActivity',
+          ],
           matches:
             '[text="添加抖音商城到桌面"] +2 LinearLayout > [text="不感兴趣"]',
-          snapshotUrls: 'https://i.gkd.li/i/13669682',
+          snapshotUrls: [
+            'https://i.gkd.li/i/13669682',
+            'https://i.gkd.li/i/14740312',
+          ],
+        },
+        {
+          key: 2,
+          name: '搜索组件',
+          quickFind: true,
+          activityIds:
+            'com.ss.android.ugc.aweme.search.activity.SearchResultActivity',
+          matches: '[text="暂不开启"][clickable=true]',
+          snapshotUrls: 'https://i.gkd.li/i/14325749',
         },
       ],
     },
