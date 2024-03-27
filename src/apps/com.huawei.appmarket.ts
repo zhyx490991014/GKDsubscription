3931d28a5948f7528dc1162b44fd4ac339e33170
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-03-27 22:10:39 +0800

    feat(华为应用市场): 功能类-应用更新页面-点击[查看更多]

--- src/apps/com.huawei.appmarket.ts
+++ src/apps/com.huawei.appmarket.ts
@@ -3,17 +3,24 @@ import { defineAppConfig } from '../types';
 export default defineAppConfig({
   id: 'com.huawei.appmarket',
   name: '华为应用市场',
+  deprecatedKeys: [1],
   groups: [
     {
-      key: 1,
-      name: '青少年模式',
+      key: 2,
+      name: '功能类-应用更新页面-点击[查看更多]',
       quickFind: true,
-      matchTime: 10000,
-      resetMatch: 'app',
       actionMaximum: 1,
-      rules:
-        '[text="儿童模式"] <<n [id="android:id/topPanel"] +2 [id="android:id/buttonPanel"] >2 [text="知道了"]',
-      snapshotUrls: 'https://i.gkd.li/i/14222901',
+      resetMatch: 'app',
+      rules: [
+        {
+          activityIds:
+            'com.huawei.appmarket.service.appmgr.view.activity.AppUpdateActivity',
+          matches: '[text="查看更多"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/e2283b1d-3041-49c5-aca2-7c165c83ce37',
+          snapshotUrls: 'https://i.gkd.li/i/14738587',
+        },
+      ],
     },
   ],
 });
