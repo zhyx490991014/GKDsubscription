810c97b2c841205251c0f031521062fdadb18ed0
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-03-27 22:34:41 +0800

    feat(QQ阅读): 局部广告-右下角悬浮卡片广告、分段广告-阅读页面广告

---
+++ src/apps/com.qq.reader.ts
@@ -0,0 +1,59 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.qq.reader',
+  name: 'QQ阅读',
+  groups: [
+    {
+      key: 1,
+      name: '局部广告-右下角悬浮卡片广告',
+      desc: '点击关闭',
+      rules: [
+        {
+          quickFind: true,
+          activityIds: 'com.qq.reader.activity.MainActivity',
+          matches: '[id="com.qq.reader:id/operating_activity_close"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/17b6444c-e275-4ee4-b1d7-bd7369e08e55',
+          snapshotUrls: 'https://i.gkd.li/i/14140100',
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '分段广告-阅读页面广告',
+      desc: '点击关闭-点击[关闭本条广告]',
+      quickFind: true,
+      activityIds: 'com.qq.reader.activity.ReaderPageActivity',
+      rules: [
+        {
+          key: 0,
+          matches: '@[clickable=true] > [text="关闭广告"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/70e15d7d-0911-44e7-b0d6-a8e74c00b8b1',
+          snapshotUrls: 'https://i.gkd.li/i/14767123',
+        },
+        {
+          key: 1,
+          matches: '[vid="adv_close"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/eb2bca3c-abbc-4666-b505-cdaeeec6a839',
+          snapshotUrls: 'https://i.gkd.li/i/14767126',
+        },
+
+        // 预留 key
+        {
+          preKeys: [0, 1],
+          key: 50,
+          matches: '[vid="view_close_ad_btn_bg"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/c985da66-c2cc-4400-bb61-8f7ee6dbba11',
+          snapshotUrls: [
+            'https://i.gkd.li/i/14767165',
+            'https://i.gkd.li/i/14767124',
+          ],
+        },
+      ],
+    },
+  ],
+});
