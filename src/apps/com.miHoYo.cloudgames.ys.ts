a85d90821f81cd53b6d9fd940baa163dc49fc55d
ZYX <74281356+zhyx490991014@users.noreply.github.com>
2024-03-28 13:19:38 +0800

    加上quickFind

--- src/apps/com.miHoYo.cloudgames.ys.ts
+++ src/apps/com.miHoYo.cloudgames.ys.ts
@@ -8,6 +8,7 @@ export default defineAppConfig({
       key: 1,
       name: '功能类-流量提醒',
       desc: '点击[使用流量进行游戏]',
+      quickFind: true,
       activityIds: 'com.mihoyo.cloudgame.main.MiHoYoCloudMainActivity',
       rules:
         'TextView[id="com.miHoYo.cloudgames.ys:id/cancelTv"][text="使用流量进行游戏"][visibleToUser=true]',
